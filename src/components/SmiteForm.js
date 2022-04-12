359ec120956d2aa74f44ee8f2b99c88ffa60a7a8
Gus Beringer <berin013@umn.edu>
2022-04-12 16:53:53 -0500

    write form settings to local storage

--- src/components/SmiteForm.js
+++ src/components/SmiteForm.js
@@ -1,26 +1,63 @@
-import React from 'react'
-import { useForm } from 'react-hook-form'
-import Button from './Button'
-import './SmiteForm.css'
+import React from "react";
+import { useForm } from "react-hook-form";
+import Button from "./Button";
+import "./SmiteForm.css";
 
-const SmiteForm = ({childToParent}) => {
-  
-  const {register, handleSubmit, formState: {errors}} = useForm()
+const SmiteForm = ({ childToParent }) => {
+  const {
+    register,
+    handleSubmit,
+    formState: {errors},
+    getValues,
+    setValue
+  } = useForm();
+
+  const setData = () => {
+    const form_json = JSON.stringify(getValues())
+    console.log(form_json)
+    localStorage.setItem("formData", form_json);
+  };
+
+  const getData = () => {
+    const data_json = localStorage.getItem("formData");
+    if (data_json === null)
+      return
+    const data = JSON.parse(data_json)
+    Object.keys(data).forEach(key => {
+      setValue(key, data[key])
+    })
+  };
 
   return (
     <form onSubmit={handleSubmit(childToParent)}>
+      <button
+        onClick={(event) => {
+          event.preventDefault();
+          getData();
+        }}
+      >
+        GET
+      </button>
+      <button
+        onClick={(event) => {
+          event.preventDefault();
+          setData();
+        }}
+      >
+        SET
+      </button>
       <div className="item">
         <label forhtml="swordDamage">Sword Damage</label>
-        <input 
+        <input
           {...register("swordDamage", {
             required: "Sword damage is required.",
             pattern: {
               value: /^(\d{1,2})d(\d{1,2})(\+\d+)?$/,
-              message: "Incorrect dice format."
-            }
+              message: "Incorrect dice format.",
+            },
           })}
-          type="text" 
-          placeholder='1d8'
+          type="text"
+          placeholder="1d8"
         />
         {errors.swordDamage && (
           <small className="text-danger">{errors.swordDamage.message}</small>
@@ -28,44 +65,60 @@ const SmiteForm = ({childToParent}) => {
       </div>
       <div className="item">
         <label forhtml="smite-level-label">Smite Level</label>
-        <select
-          label="smite-level-label"
-          {...register("smiteLevel")}
-        >
-        <option value={0}>None</option>
-        <option value={1}>1</option>
-        <option value={2}>2</option>
-        <option value={3}>3</option>
-        <option value={4}>4</option>
-        <option value={5}>5</option>
-      </select>
+        <select label="smite-level-label" {...register("smiteLevel")}>
+          <option value={0}>None</option>
+          <option value={1}>1</option>
+          <option value={2}>2</option>
+          <option value={3}>3</option>
+          <option value={4}>4</option>
+          <option value={5}>5</option>
+        </select>
       </div>
 
       <div className="item">
         <label forhtml="special-label">Fiend/Undead</label>
-        <input {...register("special")} type="checkbox" label="special-label" className='checkmark'/>
+        <input
+          {...register("special")}
+          type="checkbox"
+          label="special-label"
+          className="checkmark"
+        />
       </div>
 
       <div className="item">
         <label forhtml="critical-label">Critical Hit</label>
-        <input {...register("critical")} type="checkbox" label="critical-label" className='checkmark'/>
+        <input
+          {...register("critical")}
+          type="checkbox"
+          label="critical-label"
+          className="checkmark"
+        />
       </div>
 
       <div className="item">
         <label forhtml="improved-label">Improved Divine Smite</label>
-        <input {...register("improved")} type="checkbox" label="improved-label" className='checkmark'/>
+        <input
+          {...register("improved")}
+          type="checkbox"
+          label="improved-label"
+          className="checkmark"
+        />
       </div>
 
       <div className="item">
         <label forhtml="holy-label">Holy Avenger</label>
-        <input {...register("holy")} type="checkbox" label="holy-label" className='checkmark'/>
+        <input
+          {...register("holy")}
+          type="checkbox"
+          label="holy-label"
+          className="checkmark"
+        />
       </div>
 
       <Button>Roll Damage</Button>
       {/* <button className="btn">Roll Damage</button> */}
-
     </form>
-  )
-}
+  );
+};
 
-export default SmiteForm
+export default SmiteForm;
